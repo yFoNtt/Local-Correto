3681a6367987c06360f3983b9649742e368bb22d
João Victor <154566658+kkkjjoao@users.noreply.github.com>
2024-06-17 00:15:08 -0300

    todos os show.Modal do javascript

---
+++ script.js
@@ -0,0 +1,64 @@
+function mostraCaiua() {
+    const caiua = document.querySelector("#caiua");
+    caiua.showModal();
+}
+
+function mostraCajuru() {
+    const cajuru = document.querySelector("#cajuru");
+    cajuru.showModal();
+}
+
+function mostraCampoSantana() {
+    const campoSantana = document.querySelector("#campoSantana");
+    campoSantana.showModal();
+}
+
+function mostraCIC() {
+    const CIC = document.querySelector("#CIC");
+    CIC.showModal();
+}
+
+function mostraericoVerissimo() {
+    const ericoVerissimo = document.querySelector("#ericoVerissimo");
+    ericoVerissimo.showModal();
+}
+
+function mostraGuacui() {
+    const guacui = document.querySelector("#guacui");
+    guacui.showModal();
+}
+
+function mostraIcarai() {
+    const icarai = document.querySelector("#icarai");
+    icarai.showModal();
+}
+
+
+function mostraJandaia() {
+    const jandaia = document.querySelector("#jandaia");
+    jandaia.showModal();
+}
+
+
+function mostraMetropolitano() {
+    const metropolitano = document.querySelector("#metropolitano");
+    metropolitano.showModal();
+}
+
+
+function mostraGomm() {
+    const gomm = document.querySelector("#gomm");
+    gomm.showModal();
+}
+
+
+function mostraSambaqui() {
+    const sambaqui = document.querySelector("#sambaqui");
+    sambaqui.showModal();
+}
+
+function mostraVilaNova() {
+    const vilaNova = document.querySelector("#vilaNova");
+    vilaNova.showModal();
+}
+
